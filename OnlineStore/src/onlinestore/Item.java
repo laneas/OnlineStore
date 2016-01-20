a897dfee116d6d5bcf9eb77c03437cad8eb947c9
laneas <ardjenlane@hotmail.com>
2016-01-19 19:09:48 -0500

    item and transaction added

---
+++ OnlineStore/src/onlinestore/Item.java
@@ -0,0 +1,80 @@
+package onlinestore;
+
+public class Item
+{
+    private int id;
+    private double price;
+    private String name;
+    private String description;
+    
+    public Item(int theID, double thePrice, String theName)
+    {
+        id = theID;
+        price = thePrice;
+        name = theName;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice()
+    {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+}
---
+++ OnlineStore/src/onlinestore/Transaction.java
@@ -0,0 +1,12 @@
+package onlinestore;
+
+public class Transaction
+{
+    //private Customer customer;
+    private int transactionID;
+    
+    public Transaction(int transNum)
+    {
+        transactionID = transNum;
+    }
+}
